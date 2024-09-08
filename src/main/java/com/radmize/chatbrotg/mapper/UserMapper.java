1859d97bbcab6be0141d2a985408e6e674d93e17
Rafael <kunakov.rafael@gmail.com>
2024-09-08 20:09:48 +0300

    GOAPI-1990 тестовы коммит

--- src/main/java/com/radmize/chatbrotg/mapper/UserMapper.java
+++ src/main/java/com/radmize/chatbrotg/mapper/UserMapper.java
@@ -5,7 +5,7 @@
 //import org.mapstruct.Mapper;
 //import org.mapstruct.Mapping;
 //
-//@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring")//
 //public interface UserMapper {
 //    @Mapping(target = "id", ignore = true)
 //    User toUser(UserDto user);
